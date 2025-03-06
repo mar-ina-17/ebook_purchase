1f0fb7cbbb0e38e5f429dd5b2bd51090193943a5
mar-ina-17 <marina.workplace@gmail.com>
2025-03-06 18:14:50 +0200

    created & styled the hero section; added the assets and the styles for the project

---
+++ src/lib/components/HeroSection.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+	import bookCover from '$assets/book_cover.png';
+	import phoneCover from '$assets/phone_cover.png';
+</script>
+
+<section class="hero">
+	<div class="hero-showcase text-center bold">
+		<h1 class="purple">Move to</h1>
+		<h1 class="purple mb-xs">SPAIN</h1>
+		<h4 class="italic semi-bold">Aren't you already packing up?</h4>
+		<img class="book-cover" src={bookCover} alt="book-cover" />
+		<img class="phone-cover" src={phoneCover} alt="phone-cover" />
+	</div>
+	<div class="hero-text white text-center">
+		<h1>THIS EBOOK WILL</h1>
+		<h1>SAVE YOU MONEY,</h1>
+		<h1 class="mb-m">TIME & NERVES</h1>
+		<p class="light-grey">
+			In this book, we'll help you plan your perfect relocation to Spain, including everything from
+			accommodations to transportation. We'll also cover essential travel tips, such as budgeting,
+			packing lists, and planning your itinerary.
+		</p>
+	</div>
+</section>
+
+<style>
+	.hero {
+		display: flex;
+		height: 100vh;
+		overflow-y: hidden;
+	}
+
+	.hero-showcase {
+		position: relative;
+		width: 50%;
+		padding: 100px 16px 0 16px;
+		background: linear-gradient(135deg, #e2cbff, #e2cbff 25%, #deeaff 50%, #ece5ff 75%, #927bce);
+	}
+
+	.hero-text {
+		padding: 100px 5%;
+		width: 50%;
+		background-color: black;
+	}
+
+	.book-cover {
+		position: absolute;
+		right: 2%;
+		bottom: 0;
+		width: 65%;
+		transform: translateY(30%);
+	}
+
+	.phone-cover {
+		position: absolute;
+		width: 35%;
+		bottom: 0;
+		right: 52%;
+		transform: translateY(25%);
+	}
+</style>
